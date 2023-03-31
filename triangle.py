f84d9c14fb726d71172585200125adfabe665f65
saranya <saransaranya1004@gmail.com>
2023-03-30 20:36:04 -0700

    my_python_course

---
+++ triangle.py
@@ -0,0 +1,15 @@
+# *
+# **
+# ***
+# ****
+# *****
+
+n=int(input("Enter your triangle size:"))
+i=0
+while i<n:
+    j=1
+    while j<n:
+        print("*",end="")
+        j+=1
+    print()
+    i+=1    
