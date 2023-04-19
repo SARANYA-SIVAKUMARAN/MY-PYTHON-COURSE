fbd8e31bc5c6b76c7ed32849d146ec81ee776384
SARANYA_SIVAKUMARAN <saransaranya1004@gmail.com>
2023-04-19 09:09:04 +0530

    My python course

--- stud_fun.py
+++ stud_fun.py
@@ -39,7 +39,56 @@ def view_student():
         sno+=1    
     print(tabulate(table,headers="firstrow"))
     
-# def update_student():
+def update_student():
+    view_student()
+    update_student =read_file()
+    update_stud=input("Enter the serial number of student for updation : ")
+    choice=input("1.Name\t2.Age\t3.Address\t4.Course\t5.All\nWhat you want to update : ")
+    sno=1
+    for update in update_student['stud_data']:
+        if str(sno)==update_stud:
+            if choice=="1":
+                name=input("enter updated name : ")
+                update['name']=name
+                print("Updated Sucessfully")
+            elif choice=="2":
+                age=input("enter updated age : ")
+                update['age']=age
+                print("Updated Sucessfully")
+            elif choice=="3":
+                add=input("enter updated address : ")
+                update['add']=add
+                print("Updated Sucessfully")
+            elif choice=="4":
+                course=input("enter updated course : ")
+                update['course']=course
+                print("Updated Sucessfully")
+            elif choice=="5":
+                name=input("enter updated name : ")
+                update['name']=name
+                age=input("enter updated age : ")
+                update['age']=age  
+                add=input("enter updated address : ")
+                update['add']=add
+                course=input("enter updated course : ")
+                update['course']=course
+                print("Updated Sucessfully")
+                
+            else:
+                print("invalid choice")
+                break    
+        sno+=1  
+    write_file(update_student)  
     
-        
+def delete_student():
+    delete_student=read_file()
+    view_student()
+    choice=input("Enter the serial number of student you want to delete : ")
+    sno=1
+    for delete in delete_student['stud_data']:
+        if str(sno)==choice:
+            delete_student['stud_data'].remove(delete)
+            print("Deleted Sucessfully")
+        sno+=1
+    write_file(delete_student)                 
     
